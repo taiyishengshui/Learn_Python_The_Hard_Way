dd5020a4217d4d28aa671377cd84df634686c62b
youtanhuakai <brant_wu@163.com>
2018-02-07 16:10:03 +0800

    ex26.py

--- ex26.py
+++ ex26.py
@@ -1,6 +1,9 @@
+from sys import argv
+
 print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
+height = input()
 print("How much do you weigh?", end=' ')
 weight = input()
 
@@ -8,18 +11,19 @@ print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 script, filename = argv
 
-txt = open(filenme)
+txt = open(filename)
 
-print("Here's your file {filename}:")
-print(tx.read())
+print(f"Here's your file {filename}:")
+print(txt.read())
+txt.close()
 
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
-print(txt_again_read())
-
+print(txt_again.read())
+txt_again.close()
 
 print("Let's practice everything.")
 print('''You\'d need to know \'bout escapes
@@ -40,18 +44,18 @@ print(poem)
 print("--------------")
 
 
-five = 10 - 2 + 3 - 
-print(f"This should be five: {five}"
+five = 10 - 2 + 3 - 6
+print(f"This should be five: {five}")
 
-def secret_formula(started)
+def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars  100
+    crates = jars  / 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-beans, jars = secret_formula(start_point)
+beans, jars, crates= secret_formula(start_point)
 
 # remember that this is another way to format a string
 print("With a starting point of: {}".format(start_point))
@@ -61,39 +65,34 @@ print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
 
 
 people = 20
-cates = 30
+cats = 30
 dogs = 15
 
 
 if people < cats:
-    print "Too many cats! The world is doomed!"
-
-if people < cats:
+    print("Too many cats! The world is doomed!")
+else:
     print("Not many cats! The world is saved!")
 
 if people < dogs:
     print("The world is drooled on!")
-
-if people > dogs
+else:
     print("The world is dry!")
-
 
 dogs += 5
 
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs
-    print("People are less than or equal to dogs.)
-
-
-if people = dogs:
+elif people < dogs:
+    print("People are less than or equal to dogs.")
+else:
     print("People are dogs.")
 
